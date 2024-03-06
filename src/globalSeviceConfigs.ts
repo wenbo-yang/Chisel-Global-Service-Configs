397009a92934a331485390d7342534eb55c84ba5
wenbo yang <wenbo.yang@wbd.com>
2024-03-06 14:17:45 -0800

    push

--- src/globalSeviceConfigs.ts
+++ src/globalSeviceConfigs.ts
@@ -1,14 +1,7 @@
-export interface ServicePorts {
-    http: number;
-    https: number;
-}
-
-
+import { ServicePorts, serviceMapping } from "./globalSerivceConfigMapping";
 
 export class GlobalServiceConfigs {
-    
-
     public getServicePorts(serviceName: string, env: string): ServicePorts {
-
+        return serviceMapping.get(serviceName)?.get(env) || {http: 5000, https: 3000} as ServicePorts;
     }
 }
---
+++ test/unit/globalServiceConfigs.spec.ts
@@ -0,0 +1,10 @@
+import {GlobalServiceConfigs} from '../../src/globalSeviceConfigs'
+
+describe('globalServiceConfigs', () => {
+    describe('getServicePorts', () => {
+        it('for skeletonizer getServicePorts for development should return 5000 and 3000', () => {
+            const servicePorts = new GlobalServiceConfigs().getServicePorts("skeletonizer", "development");
+            expect(servicePorts).toEqual({http: 5000, https:3000});
+        });
+    });
+})
