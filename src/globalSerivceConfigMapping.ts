58356730db31f7f8f10da3bd845139878984066d
wenbo yang <wenbo.yang@wbd.com>
2024-03-08 09:59:11 -0800

    prettier

--- src/globalSerivceConfigMapping.ts
+++ src/globalSerivceConfigMapping.ts
@@ -3,11 +3,12 @@ export interface ServicePorts {
     https: number;
 }
 
-export const serviceMapping: Map<string, Map<string, ServicePorts>> =  new Map(
-        [
-            ["skeletonizer", new Map([
-                ["development", {http: 5000, https: 3000}],
-                ["production", {http: 5000, https: 3000}],
-            ])]
-        ]
-);
+export const serviceMapping: Map<string, Map<string, ServicePorts>> = new Map([
+    [
+        'skeletonizer',
+        new Map([
+            ['development', { http: 5000, https: 3000 }],
+            ['production', { http: 5000, https: 3000 }],
+        ]),
+    ],
+]);
--- src/globalSeviceConfigs.ts
+++ src/globalSeviceConfigs.ts
@@ -1,7 +1,7 @@
-import { ServicePorts, serviceMapping } from "./globalSerivceConfigMapping";
+import { ServicePorts, serviceMapping } from './globalSerivceConfigMapping';
 
 export class GlobalServiceConfigs {
     public getServicePorts(serviceName: string, env: string): ServicePorts {
-        return serviceMapping.get(serviceName)?.get(env) || {http: 5000, https: 3000} as ServicePorts;
+        return serviceMapping.get(serviceName)?.get(env) || ({ http: 5000, https: 3000 } as ServicePorts);
     }
 }
