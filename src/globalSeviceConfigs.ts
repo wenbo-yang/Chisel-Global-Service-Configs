d54dae8c210fe05180ff330ebe0e0ff26a932878
wenbo yang <wenbo.yang@wbd.com>
2024-03-11 13:19:49 -0700

    merge

--- src/globalSeviceConfigs.ts
+++ src/globalSeviceConfigs.ts
@@ -1,7 +1,12 @@
-import { ServicePorts, serviceMapping } from './globalSerivceConfigMapping';
+const serviceMapping = require('./globalServicePortMappings.json');
+
+export interface ServicePorts {
+    http: number;
+    https: number;
+}
 
 export class GlobalServiceConfigs {
     public getServicePorts(serviceName: string, env: string): ServicePorts {
-        return serviceMapping.get(serviceName)?.get(env) || ({ http: 5000, https: 3000 } as ServicePorts);
+        return serviceMapping.hasOwnProperty(serviceName) &&  serviceMapping[serviceName].hasOwnProperty(env) ? serviceMapping[serviceName][env] : ({ http: 5000, https: 3000 } as ServicePorts);
     }
 }
