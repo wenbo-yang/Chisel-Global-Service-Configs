5626ec38f1979593c21cf0424f14d72b09f89b85
wenbo yang <wenbo.yang@wbd.com>
2024-03-13 11:04:51 -0700

    remove code from config

--- test/unit/globalServiceConfigs.spec.ts
+++ test/unit/globalServiceConfigs.spec.ts
@@ -1,10 +1,24 @@
-import { GlobalServiceConfigs } from '../../src/globalSeviceConfigs';
+import * as globalServicePortMappings from '../../configs/globalServicePortMappings.json';
+
+interface ServicePorts {
+    http: number;
+    https: number;
+}
+
+function getServicePorts(serviceName: string, env: string): ServicePorts {
+    return globalServicePortMappings.hasOwnProperty(serviceName) && (globalServicePortMappings as any)[serviceName].hasOwnProperty(env) ? (globalServicePortMappings as any)[serviceName][env] : ({ http: -1, https: -1 } as ServicePorts);
+}
 
 describe('globalServiceConfigs', () => {
     describe('getServicePorts', () => {
         it('for skeletonizer getServicePorts for development should return 5000 and 3000', () => {
-            const servicePorts = new GlobalServiceConfigs().getServicePorts('skeletonizer', 'development');
+            const servicePorts = getServicePorts('skeletonizer', 'development');
             expect(servicePorts).toEqual({ http: 5000, https: 3000 });
         });
+
+        it('for character-training service developement should return 5001 and 3001', () => {
+            const servicePorts = getServicePorts('character-training', 'development');
+            expect(servicePorts).toEqual({ http: 5001, https: 3001 });
+        })
     });
 });
